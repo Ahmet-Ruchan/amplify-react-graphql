6d4c900cebabdf8848a0db66ce5b82abeea2c7ac
Ahmet Ruçhan Avcı <Ahmet Ruchan Avci>
2023-02-22 12:00:30 +0300

    yeni

---
+++ amplify-react-graphql-master/pages/_document.js
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;900&display=swap" rel="stylesheet"/>
+      </Head>
+      <body className="font-poppins">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
